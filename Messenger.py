7315662a58424a267c3f59abe49db81d89d0b8a1
leighaVanOrsdol <324560543+Leigha-VanOrsdol@users.noreply.github.com>
2026-09-11 14:31:05 -0500

    text file connection made

---
+++ Messenger.py
@@ -0,0 +1,14 @@
+name = "Leigha VanOrsdol"
+
+message_file = input("What is the message file name? (file_name.txt) ")
+
+with open(message_file, "a") as file:
+    print("Got it!")
+    
+with open(message_file, "r") as file:
+    print(file.read())
+    
+message = input("What would you like to say? ")
+
+with open(message_file, "a") as file:
+    file.write(f"[{name}]: {message}")
